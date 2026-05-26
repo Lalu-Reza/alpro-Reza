eea40f328434ab8686951cc2f5819341b41d0315
Lalu Reza <lalurezapramandika@gmail.com>
2026-05-26 20:47:05 +0800

    jurnal sementara modul 6

--- MODUL-6/Soal1.cpp
+++ MODUL-6/Soal1.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
-	
-	
-	return 0;
+int main(int argc, char* argv[]) {
+   if (argc > 2){
+    cout << "Error: Too many parameters. Connection unstable." << endl;
+    return 1;
+   }
+   int panjang;
+   while (*argv[panjang] !='\0'){
+    panjang++;
+   }
+   if (panjang != 11){
+    cout << "Error: Operator ID must be exactly 11 characters long." << endl;
+   }
+    if (*argv[0] != 'F'){
+        if (*argv[1] != '1'){
+            if(*argv[2] != 'D'){
+                if(*argv[3] != '0'){
+                    if(*argv[4] != '2'){
+                    cout << "Error: Too many parameters. Connection unstable." << endl;
+                    return 1;
+                    }
+                }
+            }
+        }
+    }
+    
+	cout << "Berhasil" << endl;
+    return 0;
 }
